bf3a31b84810b34b2b555350a6bffcb6b0b05313
Joseph J Helfenbein <josephhelfenbein@gmail.com>
2025-01-25 08:01:53 -0500

    clerk provider navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,15 @@
 "use client";
 
-import { ClerkProvider } from '@clerk/nextjs';
 import './globals.css';
 import React from 'react';
 
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <ClerkProvider>
       <html lang="en">
         <body className="centered-content">
           {children}
         </body>
       </html>
-    </ClerkProvider>
   );
 }
