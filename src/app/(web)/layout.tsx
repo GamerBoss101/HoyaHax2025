98161b11db1dfe75e0d91f6e8fb751c219227e11
Joseph J Helfenbein <josephhelfenbein@gmail.com>
2025-01-25 07:28:14 -0500

    add import react

--- src/app/(web)/layout.tsx
+++ src/app/(web)/layout.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import React from 'react';
 import { ClerkProvider } from '@clerk/nextjs';
 import { Navbar } from '@/components/navbar';
 import { Footer } from '@/components/footer';
@@ -7,11 +8,7 @@ import { ThemeProvider } from '@/components/theme-provider';
 
 import '../globals.css'
 
-export default function RootLayout({
-	children,
-}: {
-	children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<ClerkProvider>
 			<html lang="en">
