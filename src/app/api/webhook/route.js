527f955870be678c12ae38885d5f1768a7cfd195
Joseph J Helfenbein <josephhelfenbein@gmail.com>
2025-01-25 05:01:15 -0500

    include connectDB in route file

--- src/app/api/webhook/route.js
+++ src/app/api/webhook/route.js
@@ -2,8 +2,41 @@ import { User } from '../../../models/User';
 import { NextResponse } from 'next/server';
 import { Webhook } from 'svix';
 import connectDB from '../../lib/connectDB';
+import mongoose from "mongoose";
 
 const CLERK_WEBHOOK_SECRET = process.env.CLERK_WEBHOOK_SECRET;
+
+const DATABASE_URL = process.env.MONGO_URI;
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+async function connectDB() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+
+    try {
+      cached.promise = mongoose.connect(DATABASE_URL, opts).then((mongoose) => {
+        console.log('MongoDB connected successfully');
+        return mongoose;
+      });
+    } catch (error) {
+      console.error('Error connecting to MongoDB:', error.message);
+      throw new Error('Error connecting to MongoDB');
+    }
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
 
 export async function POST(req) {
   console.log('Received request:', req);
