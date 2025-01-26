a5886f3da568e544c586b2febc97020bb5766039
Sir Blob <76974209+GamerBoss101@users.noreply.github.com>
2025-01-25 20:49:38 -0500

    Package Update

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -3,7 +3,6 @@
 import { Hero } from "@/components/hero";
 import { Facts } from "@/components/facts";
 
-import SignupPage from './login/page'
 import Link from "next/link";
 
 export default function Home() {
@@ -14,8 +13,6 @@ export default function Home() {
 			<Facts />
 
 			<div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
-				<h1 style={{ fontSize: 64, marginBottom: 20 }}>Welcome to Hoya</h1>
-				<SignupPage />
 				<Link href="/transcribe">
 					<button>Go to Transcribe Page</button>
 				</Link>
--- src/components/facts.tsx
+++ src/components/facts.tsx
@@ -1,19 +1,20 @@
 "use client"
-import { Card, CardContent } from "./ui/card"
 
 export function Facts() {
     return (
         <section className="mx-auto my-auto bg-white dark:bg-gray-900">
-            <Card>
-                <CardContent>
-                    <h2>
-                        OUR MISSION
-                    </h2>
-                    At PostCare we want to ensure the health of those throughout the world.
+        <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+            <div className="mr-auto place-self-center lg:col-span-7">
+                <h1 className=" max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
+                    Our Mission
+                </h1>
+                <p className="float-right max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+                At PostCare we want to ensure the health of those throughout the world.
                     Our goal is to make sure that our services can ensure clarity and accessibility
                     <center>As well as a smooth experience</center>
-                </CardContent>
-            </Card>
-        </section>
+                </p>
+            </div>
+        </div>
+    </section>
     )
 }
